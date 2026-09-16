ba23a383b27cf892a270c0b60726f261a2f25913
elijahpaulatem15-gif <elijahpaulatem15@gmail.com>
2026-09-16 22:22:44 +0200

    Add Rate Check service worker
    Add service worker caching to support Rate Check as an installable web app.

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "rate-check-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./file_000000000b20820bb7603403fb4c04a1.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+  );
+});
